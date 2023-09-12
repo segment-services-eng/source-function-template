2e4806cdc86c7ffae9fccc6be93d7d3d98d03328
Jason Sooter <jason.sooter@gmail.com>
2023-09-11 18:06:38 -0600

    feat(test): add `Segment` global variable with all spec methods

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@
  * @param  {FunctionRequest} request
  * @param  {FunctionSettings} settings
  */
-async function onRequest(event, settings) {
+async function onRequest(request, settings) {
   const body = request.json();
 
-  const endpoint = ""; // replace with your endpoint
+  const endpoint = ''; // replace with your endpoint
   let response;
 
   try {
     response = await fetch(endpoint, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Authorization: `Basic ${btoa(settings.apiKey + ":")}`,
-        "Content-Type": "application/json",
+        Authorization: `Basic ${btoa(settings.apiKey + ':')}`,
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(body),
+      body: JSON.stringify(body)
     });
   } catch (error) {
     // Retry on connection error
@@ -34,56 +34,56 @@ async function onRequest(event, settings) {
 
   // See https://segment.com/docs/connections/spec/track/
   Segment.track({
-    event: "Test Event",
-    userId: "user_id",
+    event: 'Test Event',
+    userId: 'user_id',
     properties: {
-      testProperty: "testValue",
-      testProperty2: response.propertyName,
-    },
+      testProperty: 'testValue',
+      testProperty2: response.propertyName
+    }
   });
 
   // See https://segment.com/docs/connections/spec/identify/
   Segment.identify({
-    userId: "user_id",
+    userId: 'user_id',
     traits: {
-      userName: "Unicorn",
-    },
+      userName: 'Unicorn'
+    }
   });
 
   // See https://segment.com/docs/connections/spec/group/
   Segment.group({
-    groupId: "group_id",
-    userId: "user_id",
+    groupId: 'group_id',
+    userId: 'user_id',
     traits: {
-      groupName: "Unicorn",
-    },
+      groupName: 'Unicorn'
+    }
   });
 
   // See https://segment.com/docs/connections/spec/page/
   Segment.page({
-    name: "Home page",
-    userId: "user_id",
+    name: 'Home page',
+    userId: 'user_id',
     properties: {
-      url: "https://mywebsite.com/about",
-    },
+      url: 'https://mywebsite.com/about'
+    }
   });
 
   // See https://segment.com/docs/connections/spec/screen/
   Segment.screen({
-    name: "Test Screen",
-    userId: "user_id",
+    name: 'Test Screen',
+    userId: 'user_id',
     properties: {
-      url: "https://mywebsite.com/about",
-    },
+      url: 'https://mywebsite.com/about'
+    }
   });
 
   // See https://segment.com/docs/connections/sources/catalog/libraries/server/object-api/
   Segment.set({
-    collection: "users",
-    id: "user_id",
+    collection: 'users',
+    id: 'user_id',
     properties: {
-      userName: "Unicorn",
-    },
+      userName: 'Unicorn'
+    }
   });
 }
 
@@ -91,7 +91,7 @@ async function onRequest(event, settings) {
  * Exports for Testing Only
  */
 try {
-  if (process?.env["NODE_DEV"] === "TEST") {
+  if (process?.env['NODE_DEV'] === 'TEST') {
     module.exports = {
       onRequest
     };
--- src/index.test.js
+++ src/index.test.js
@@ -4,10 +4,56 @@ const { onRequest } = require('./index.js');
 jest.spyOn(global.console, 'log').mockImplementation();
 jest.spyOn(global.console, 'error').mockImplementation();
 
+/**
+ * Spy on Segment Methods
+ */
+jest.spyOn(global.Segment, 'track').mockImplementation();
+jest.spyOn(global.Segment, 'identify').mockImplementation();
+jest.spyOn(global.Segment, 'page').mockImplementation();
+jest.spyOn(global.Segment, 'group').mockImplementation();
+jest.spyOn(global.Segment, 'screen').mockImplementation();
+jest.spyOn(global.Segment, 'set').mockImplementation();
+
+const baseRequest = {
+  json: () => {}
+};
+
+const baseSettings = {};
+
 describe('onRequest', () => {
-  it('should call console.log twice', async () => {
-    expect.assertions(1);
-    await onRequest({}, {});
-    expect(console.log).toHaveBeenCalledTimes(2);
+  it('should call fetch & Segment.track', async () => {
+    expect.assertions(2);
+
+    /**
+     * Call `onRequest`
+     */
+    await onRequest(baseRequest, baseSettings);
+
+    /**
+     * Expect `fetch` to have been called
+     */
+    expect(fetch.mock.calls).toHaveLength(1);
+
+    /**
+     * Expect `Segment.track` to have been called
+     */
+    expect(Segment.track).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call remaining Segment methods', async () => {
+    expect.assertions(4);
+
+    /**
+     * Call `onRequest`
+     */
+    await onRequest(baseRequest, baseSettings);
+
+    /**
+     * Expect `Segment` methods to have been called
+     */
+    expect(Segment.identify).toHaveBeenCalledTimes(1);
+    expect(Segment.track).toHaveBeenCalledTimes(1);
+    expect(Segment.track).toHaveBeenCalledTimes(1);
+    expect(Segment.track).toHaveBeenCalledTimes(1);
   });
 });
